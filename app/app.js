6698f44af25974188c4c522410613e35fe1e74f3
Adam Waselnuk <adam.waselnuk@gmail.com>
2016-09-07 21:53:34 -0400

    Adding watch setup and dom ready function

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
+'use strict';
+
 function ready(fn) {
-  if (document.readyState != 'loading'){
+  if (document.readyState != 'loading') {
     fn();
   } else {
     document.addEventListener('DOMContentLoaded', fn);
@@ -7,7 +9,7 @@ function ready(fn) {
 }
 
 function main() {
-  const runButton = document.getElementById('run');
+  var runButton = document.getElementById('run');
 
   runButton.addEventListener('click', function () {
     console.log('it has been watched');
